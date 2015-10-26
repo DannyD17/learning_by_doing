2136bd8d7e10a21bc43e128774dde3548d1082ed
Danny Debevec <daniel.debevec@gmail.com>
2015-10-25 18:33:45 -0600

    Updated string intoploation to not use math or method calls
    [#105491216]

--- objects/cash_register.rb
+++ objects/cash_register.rb
@@ -15,11 +15,12 @@ class CashRegister
 
   def pay(amount_paid)
     if amount_paid > @total
-      puts "Your change is $#{currency((@total - amount_paid).abs)}"
+      change = currency(@total - amount_paid).abs
+      puts "Your change is $#{change}"
       @total = 0.00
     else
-      puts "Your new total is $#{currency(@total - amount_paid)}"
       @total -= amount_paid
+      puts "Your new total is $#{@total}"
     end
   end
 
@@ -29,18 +30,18 @@ class CashRegister
 end
 
 register = CashRegister.new
-puts register.total  # => 0.00
-puts register.purchase(3.78)  # => 3.78
-puts register.total  # => 3.78
-register.pay(5.00)  # => "Your change is $1.22"
+puts register.total # => 0.00
+puts register.purchase(3.78) # => 3.78
+puts register.total # => 3.78
+register.pay(5.00) # => "Your change is $1.22"
 puts register.total # => 0.00
 puts ''
 register = CashRegister.new
-puts register.total  # => 0.00
-puts register.purchase(3.78)  	# => 3.78
-puts register.purchase(5.22)	# => 9.00
-puts register.total  			# => 9.00
-register.pay(5.00)  # => "Your new total is $4.00"
-puts register.total			# => 4.00
-register.pay(5.00)  # => "Your change is $1.00"
+puts register.total # => 0.00
+puts register.purchase(3.78) # => 3.78
+puts register.purchase(5.22) # => 9.00
+puts register.total # => 9.00
+register.pay(5.00) # => "Your new total is $4.00"
+puts register.total # => 4.00
+register.pay(5.00) # => "Your change is $1.00"
 puts register.total # => 0.00
